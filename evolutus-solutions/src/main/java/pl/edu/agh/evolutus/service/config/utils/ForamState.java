787de7422fc312bdbe63e156a2663433ea005b51
maciej.bassara <mbassara@gmail.com>
2015-06-24 22:56:51 +0200

    refs #34 Extracted following methods from Foram class to config.
    * shouldDie()
    * canReproduce()
    * canCreateChamber()
    * canMigrate()

--- evolutus-solutions/src/main/java/pl/edu/agh/evolutus/service/config/utils/ForamState.java
+++ evolutus-solutions/src/main/java/pl/edu/agh/evolutus/service/config/utils/ForamState.java
@@ -7,16 +7,18 @@ import pl.edu.agh.evolutus.genotype.Genotype;
 
 public class ForamState {
 
+	public final boolean isBenthic;
 	public final Map<String, Double[]> genotype;
 	public final double energy;
 	public final double age;
 	public final Shell shell;
 
-	public ForamState(Genotype genotype, double energy, double age, Shell shell) {
-		this(genotype.toFossilizationMap(), energy, age, shell);
+	public ForamState(boolean isBenthic, Genotype genotype, double energy, double age, Shell shell) {
+		this(isBenthic, genotype.toFossilizationMap(), energy, age, shell);
 	}
 
-	public ForamState(Map<String, Double[]> genotype, double energy, double age, Shell shell) {
+	public ForamState(boolean isBenthic, Map<String, Double[]> genotype, double energy, double age, Shell shell) {
+		this.isBenthic = isBenthic;
 		this.genotype = genotype;
 		this.energy = energy;
 		this.age = age;
