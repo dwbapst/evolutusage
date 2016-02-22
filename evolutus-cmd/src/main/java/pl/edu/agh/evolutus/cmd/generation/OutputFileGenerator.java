1895b61be36600bcde60290b3da0e03bc0a7f1fe
topa <topa@agh.edu.pl>
2016-02-22 15:14:49 +0100

    Solution of situation when foramsCount saved in OceanFragmentInfo is equal to zero. Prevents from dividing by zero when average values are calculated.

--- evolutus-cmd/src/main/java/pl/edu/agh/evolutus/cmd/generation/OutputFileGenerator.java
+++ evolutus-cmd/src/main/java/pl/edu/agh/evolutus/cmd/generation/OutputFileGenerator.java
@@ -43,8 +43,8 @@ public abstract class OutputFileGenerator {
 				averageEnergy += info.getTotalEnergy();
 				averageShellVolume += info.getAverageShellVolume();
 			}
-			averageEnergy = averageEnergy / foramsCount;
-			averageShellVolume = averageShellVolume / foramsCount;
+			averageEnergy = (foramsCount == 0) ? 0.0 : averageEnergy / foramsCount;
+			averageShellVolume = (foramsCount == 0) ? 0.0: averageShellVolume / foramsCount;
 			statsList.add(new Stats(stepNo, foramsCount, foramsHaploidCount, foramsDiploidCount, deadForamsCount,
 					bornForamsCount,
 					algaeAvailability, averageEnergy, averageShellVolume));
